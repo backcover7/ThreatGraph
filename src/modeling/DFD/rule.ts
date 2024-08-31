4cedcfa25991649ad7444780a5c9db73b46d03c3
backcover7 <hou1417x@gmail.com>
2024-08-31 16:53:05 -0700

    add restrictions on property of rule

--- src/modeling/DFD/rule.ts
+++ src/modeling/DFD/rule.ts
@@ -2,14 +2,21 @@ import { UUID, generateUUID } from './base';
 import { Element, ElementType } from './element';
 import { Threat } from './threat';
 
+type DesignItem = {
+    designs?: DesignItem[];
+    design?: string;
+    either?: DesignItem[];
+    variable?: string;
+}
+
 // Rule Type
 export type Rule = {
     id: UUID;
     threat: Threat;
     element: ElementType | Element;
-    designs?: any;
-    either?: any;
-    design?: any;
+    designs?: DesignItem[];
+    either?: DesignItem[];
+    design?: string;
 }
 
 export const ruleSchema = {
@@ -27,30 +34,53 @@ export const ruleSchema = {
 };
 
 // Build Rule
+type BuildRuleOptions = {
+    designs?: DesignItem[];
+    either?: DesignItem[];
+    design?: string;
+    id?: UUID;
+};
+
 function buildRule(
     threat: Threat,
     element: Element | ElementType,
-    designs?: any,
-    either?: any,
-    design?: any,
-    id?: UUID | undefined): Rule {
-    return {
-        id: id !== undefined ? id : generateUUID(),
+    options: BuildRuleOptions = {}
+): Rule {
+    const { id = generateUUID() } = options;
+
+    const exclusiveProps: (keyof BuildRuleOptions)[] = ['designs', 'either', 'design'];
+    const providedProps = exclusiveProps.filter(prop => options[prop] !== undefined);
+
+    if (providedProps.length > 1) {
+        throw new Error(`Only one of designs, either, or design can be provided. Received: ${providedProps.join(', ')}`);
+    }
+
+    const rule: Rule = {
+        id,
         threat,
-        element,
-        designs,
-        either,
-        design
+        element
     };
+
+    if (options.designs !== undefined) rule.designs = options.designs;
+    else if (options.either !== undefined) rule.either = options.either;
+    else if (options.design !== undefined) rule.design = options.design;
+
+    return rule;
 }
 
-export function ruleBuilder(item: any) {
+export function ruleBuilder(item: Partial<Rule>): Rule {
+    if (!item.threat || !item.element) {
+        throw new Error('Threat and element are required');
+    }
+
     return buildRule(
         item.threat,
         item.element,
-        item.designs,
-        item.either,
-        item.design,
-        item.id
+        {
+            designs: item.designs,
+            either: item.either,
+            design: item.design,
+            id: item.id
+        }
     );
 }
