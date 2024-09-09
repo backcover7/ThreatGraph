9534add2fa0eda12c8ffcc72dbbf353ba38bc357
backcover7 <hou1417x@gmail.com>
2024-09-09 10:39:33 -0700

    group and ungroup dynamically

--- src/app/components/nodes/Zone.tsx
+++ src/app/components/nodes/Zone.tsx
@@ -9,21 +9,31 @@ interface ZoneNodeProps extends NodeProps {
     type: 'group';
 }
 
-function ZoneNode({ id, data }: ZoneNodeProps) {
+function ZoneNode({ id, data, selected }: ZoneNodeProps) {
     const {setNodes, getNodes, getInternalNode} = useReactFlow();
     getInternalNodeHook = getInternalNode;
-    const onResizeEnd = useCallback((evt: ResizeDragEvent, params: ResizeParams) => {
-        setNodes((nodes) => {
-            // debugger;
-            return groupElements(nodes);
-        });
-    }, [id, setNodes, getNodes]);
+
+    // const onResizeStart = useCallback((event: ResizeDragEvent, params: ResizeParams) => {
+    //     setNodes((nodes) => {
+    //         const detachedNode = nodes.find(dn => dn.id === id);
+    //         return detachElement(detachedNode as Node, nodes);
+    //     });
+    // }, [setNodes]);
+    //
+    //
+    // const onResizeEnd = useCallback(() => {
+    //     setNodes((nodes) => {
+    //         return groupElements(nodes);
+    //     });
+    // }, [setNodes]);
 
     return (
         <>
             <NodeResizer
                 color="#ececec"
-                onResizeEnd={onResizeEnd}
+                isVisible={selected}
+                // onResizeStart={onResizeStart}
+                // onResizeEnd={onResizeEnd}
                 // minWidth={EntityOrDatastoreWidth + 1}
                 // minHeight={EntityOrDatastoreHeight + 1}
             />
@@ -47,15 +57,20 @@ function sortZoneNodes(nodes: Node[]): Node[] {
     return [...sortedZoneNodes, ...nonZoneNodes];
 }
 
+/**
+ * Detach the node itself
+ * @param detachedNode
+ * @param nodes
+ */
 export function detachElement(detachedNode: Node, nodes: Node[]): never[] {
     return nodes.map((n: Node) => {
         if ((n as Node).id === detachedNode.id) {
-            // Calculate the new absolute position
-            const parentNode = nodes.find((pn: Node) => (pn as Node).id === (n as Node).parentId);
-            const newPosition = parentNode
+            // the element is processed is a normal node
+            const internalNode = getInternalNodeHook(detachedNode.id)
+            const newPosition = internalNode.parentId
                 ? {
-                    x: (parentNode as Node).position.x + (n as Node).position.x,
-                    y: (parentNode as Node).position.y + (n as Node).position.y
+                    x: internalNode.internals.positionAbsolute.X,
+                    y: internalNode.internals.positionAbsolute.Y,
                 }
                 : (n as Node).position;
 
@@ -71,29 +86,37 @@ export function detachElement(detachedNode: Node, nodes: Node[]): never[] {
     }) as never[];
 }
 
+/**
+ * Group nodes
+ * @param nodes
+ */
 export function groupElements(nodes: Node[]): Node[] {
-    // TODO nested zones, check from smallest zone
     nodes = sortZoneNodes(nodes);
+
     let zoneNodes = nodes.filter(node => node.type === 'group');
-    // debugger;
     zoneNodes = zoneNodes.reverse();
 
     return nodes.map(node => {
-        if (node.parentId) return node;  // skip node which has already been grouped
+        // TODO when drag or resize a zone node, the node with other parentid might be affected
+        // if (node.parentId) return node;  // skip node which has already been grouped
 
         const parentZone = zoneNodes.find(zoneNode => isNodeCompletelyInsideZone(node, zoneNode));
 
-        if (parentZone) {
+        if (parentZone && parentZone.id !== node.parentId) {
+            const internalNode = getInternalNodeHook(node.id)
+            const nodeAbsoluteX = internalNode?.internals.positionAbsolute?.x ?? node.position.x;
+            const nodeAbsoluteY = internalNode?.internals.positionAbsolute?.y ?? node.position.y;
+
             const internalZoneNode = getInternalNodeHook(parentZone.id);
-            const parentZoneX = internalZoneNode?.internals.positionAbsolute?.x ?? parentZone.position.x;
-            const parentZoneY = internalZoneNode?.internals.positionAbsolute?.y ?? parentZone.position.y;
+            const parentZoneAbsoluteX = internalZoneNode?.internals.positionAbsolute?.x ?? parentZone.position.x;
+            const parentZoneAbsoluteY = internalZoneNode?.internals.positionAbsolute?.y ?? parentZone.position.y;
             return {
                 ...node,
                 parentId: parentZone.id,
                 extent: 'parent',
                 position: {
-                    x: node.position.x - (parentZoneX ? parentZoneX : parentZone.position.x),
-                    y: node.position.y - (parentZoneY ? parentZoneY : parentZone.position.y),
+                    x: nodeAbsoluteX - (parentZoneAbsoluteX ? parentZoneAbsoluteX : parentZone.position.x),
+                    y: nodeAbsoluteY - (parentZoneAbsoluteY ? parentZoneAbsoluteY : parentZone.position.y),
                 },
             };
         }
@@ -107,22 +130,22 @@ export function isNodeCompletelyInsideZone(node: Node, zoneNode: Node): boolean 
 
     if (node.type === 'group' && getArea(node) >= getArea(zoneNode)) return false;
 
-    // const internalNode = getInternalNodeHook(node.id);
+    const internalNode = getInternalNodeHook(node.id);
     const internalZoneNode = getInternalNodeHook(zoneNode.id);
 
-    // const nodeX = internalNode?.internals.positionAbsolute?.x ?? node.position.x;
-    // const nodeY = internalNode?.internals.positionAbsolute?.y ?? node.position.y;
-    const nodeRight = node.position.x + (Number(node.measured?.width) || Number(node.style?.width) || 0);
-    const nodeBottom = node.position.y + (Number(node.measured?.height) || Number(node.style?.height) || 0);
+    const nodeAbsoluteX = internalNode?.internals.positionAbsolute?.x ?? node.position.x;
+    const nodeAbsoluteY = internalNode?.internals.positionAbsolute?.y ?? node.position.y;
+    const nodeRight = nodeAbsoluteX + (Number(node.measured?.width) || Number(node.style?.width) || 0);
+    const nodeBottom = nodeAbsoluteY + (Number(node.measured?.height) || Number(node.style?.height) || 0);
 
-    const zoneX = internalZoneNode?.internals.positionAbsolute?.x ?? zoneNode.position.x;
-    const zoneY = internalZoneNode?.internals.positionAbsolute?.y ?? zoneNode.position.y;
-    const zoneRight = zoneX + (Number(zoneNode.measured?.width) || Number(zoneNode.style?.width) || 0);
-    const zoneBottom = zoneY + (Number(zoneNode.measured?.height) || Number(zoneNode.style?.height) || 0);
+    const zoneAbsoluteX = internalZoneNode?.internals.positionAbsolute?.x ?? zoneNode.position.x;
+    const zoneAbsoluteY = internalZoneNode?.internals.positionAbsolute?.y ?? zoneNode.position.y;
+    const zoneRight = zoneAbsoluteX + (Number(zoneNode.measured?.width) || Number(zoneNode.style?.width) || 0);
+    const zoneBottom = zoneAbsoluteY + (Number(zoneNode.measured?.height) || Number(zoneNode.style?.height) || 0);
 
     return (
-        node.position.x >= zoneX &&
-        node.position.y >= zoneY &&
+        nodeAbsoluteX >= zoneAbsoluteX &&
+        nodeAbsoluteY >= zoneAbsoluteY &&
         nodeRight <= zoneRight &&
         nodeBottom <= zoneBottom
     );
