e6fc2a1b1d576583c9cfe49c853265eda1d7331d
backcover7 <hou1417x@gmail.com>
2024-09-13 14:04:21 -0700

    add built drag

--- src/app/components/toolbar/BuiltInTools.tsx
+++ src/app/components/toolbar/BuiltInTools.tsx
@@ -1,19 +1,14 @@
-'use client'
+'use client';
 
-import React, { useEffect, useState } from 'react';
-import {useDnD} from "@/app/components/DnDContext";
-import IconRenderer from "@/app/components/IconRenderer";
-import {useTemplate} from "@/app/components/toolbar/TemplateContext";
-import {NodeType} from "@/app/components/nodes/ElementNode";
+import React from 'react';
+import { useDnD } from "@/app/components/DnDContext";
+import { useTemplate } from "@/app/components/toolbar/TemplateContext";
+import { NodeType } from "@/app/components/nodes/ElementNode";
+import { FaGlobe, FaDesktop, FaDatabase, FaCogs } from 'react-icons/fa';
 
 const BuiltInTools: React.FC = () => {
     const [, , setDraggedItem] = useDnD();
     const templates = useTemplate();
-    const [loadedTemplates, setLoadedTemplates] = useState<any>({});
-
-    useEffect(() => {
-        setLoadedTemplates(templates);
-    }, [templates]);
 
     const onDragStart = (event: React.DragEvent<HTMLDivElement>, nodeType: NodeType, data: any) => {
         event.dataTransfer.setData('application/reactflow', nodeType);
@@ -21,51 +16,28 @@ const BuiltInTools: React.FC = () => {
         setDraggedItem([nodeType, data]);
     };
 
-    const renderTemplateItems = (items: any[], type: NodeType) => {
-        return items.map((item, index) => (
+    const renderToolSection = (type: keyof typeof templates, nodeType: NodeType, icon: React.ReactNode) => {
+        if (!templates[type] || templates[type].length === 0) return null;
+
+        return templates[type].map((item: any, index: number) => (
             <div
                 key={`${type}-${index}`}
                 className="dndnode"
-                onDragStart={(event) => onDragStart(event, type, item)}
+                onDragStart={(event) => onDragStart(event, nodeType, item)}
                 draggable
+                title={item.metadata?.name || 'Unnamed'}
             >
-                {item.metadata.icon ? (
-                    <IconRenderer dataUrl={item.metadata.icon} />
-                ) : (
-                    <div className="dndnode-icon">{type.charAt(0).toUpperCase()}</div>
-                )}
-                <div className="dndnode-label">{item.metadata.name}</div>
+                {icon}
             </div>
         ));
     };
 
     return (
         <div className="built-in-tools">
-            <h3>Built-in Tools</h3>
-            {loadedTemplates.zone && (
-                <div className="tool-section">
-                    <h4>Zones</h4>
-                    {renderTemplateItems(loadedTemplates.zone, 'group')}
-                </div>
-            )}
-            {loadedTemplates.entity && (
-                <div className="tool-section">
-                    <h4>Entities</h4>
-                    {renderTemplateItems(loadedTemplates.entity, 'default')}
-                </div>
-            )}
-            {loadedTemplates.datastore && (
-                <div className="tool-section">
-                    <h4>Datastores</h4>
-                    {renderTemplateItems(loadedTemplates.datastore, 'output')}
-                </div>
-            )}
-            {loadedTemplates.process && (
-                <div className="tool-section">
-                    <h4>Processes</h4>
-                    {renderTemplateItems(loadedTemplates.process, 'process')}
-                </div>
-            )}
+            {renderToolSection('zone', 'group', <FaGlobe />)}
+            {renderToolSection('entity', 'default', <FaDesktop />)}
+            {renderToolSection('datastore', 'output', <FaDatabase />)}
+            {renderToolSection('process', 'process', <FaCogs />)}
         </div>
     );
 };
