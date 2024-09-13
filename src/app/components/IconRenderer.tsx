0463c6db397d17d119703f8721ea5f09ef333dec
backcover7 <hou1417x@gmail.com>
2024-09-12 21:41:25 -0700

    fix

---
+++ src/app/components/IconRenderer.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import { sanitizeDataUrl } from "@/app/components/utils";
+
+interface IconRendererProps {
+    dataUrl?: string;
+}
+
+const IconRenderer: React.FC<IconRendererProps> = ({ dataUrl = '' }) => {
+    const [svgContent, setSvgContent] = useState<string>(dataUrl);
+    const [sanitizedUrl, setSanitizedUrl] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (svgContent) {
+            if (svgContent.startsWith('<svg')) {  // Resolve svg element
+                const svgDataUrl = `data:image/svg+xml;base64,${btoa(svgContent)}`;
+                setSanitizedUrl(sanitizeDataUrl(svgDataUrl));
+            } else {
+                setSanitizedUrl(sanitizeDataUrl(svgContent));
+            }
+        } else {
+            setSanitizedUrl(null);
+        }
+    }, [svgContent]);
+
+    if (!sanitizedUrl) {
+        return null; // TODO: image placeholder
+    }
+
+    return (
+        <img src={sanitizedUrl} alt="Icon" /> // TODO alt
+    );
+};
+
+export default IconRenderer;
